43ed765edaaf8df35e9c0d071512a3ee8a2ca663
Rashadhusien <rashadhusein440@gmail.com>
2024-06-20 17:53:02 +0300

    lesson3

--- app.js
+++ app.js
@@ -2,11 +2,22 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+const mongoose = require("mongoose");
+
 app.get("/", (req, res) => {
   res.sendFile("./views/home.html", { root: __dirname });
 });
 
-app.listen(port, () => {
-  console.log(`http://localhost:${port}/`);
-});
-// Example app listening on port ${port}
+mongoose
+  .connect(
+    `mongodb+srv://rashad:abtFF9-GNE8BAVc@cluster0.7kcudao.mongodb.net/all-data?retryWrites=true&w=majority&appName=Cluster0`
+  )
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`http://localhost:${port}/`);
+    });
+    // Example app listening on port ${port}
+  })
+  .catch((err) => {
+    console.log(err);
+  });
